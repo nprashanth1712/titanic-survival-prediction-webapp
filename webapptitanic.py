299f99a37036e82af84349e76c7ad9c543d18fac
nprashanth1712 <96611409+nprashanth1712@users.noreply.github.com>
2022-04-04 14:09:43 +0530

    Add files via upload

---
+++ webapptitanic.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  4 11:09:57 2022
+
+@author: navug
+"""
+import numpy as np
+import pickle
+import streamlit as st
+loaded_model=pickle.load(open('C:/Users/navug/Desktop/titanic ds/Titanic_trained_model.sav','rb'))
+
+def titanic_prediction(input_data):
+    
+   
+   input_data_as_numpy_array = np.asarray(input_data)
+
+
+   input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+   prediction = loaded_model.predict(input_data_reshaped)
+   print(prediction)
+
+   if (prediction[0] == 0):
+     
+     return('The person did not survive')
+   else:
+     return('The person survived')  
+ 
+    
+def main():
+    
+    st.title('Titanic Survival Prediction Web app')
+    
+    Pclass =st.text_input('Ticket class')                
+    Sex =st.text_input('Gender of the passenger')                    
+    Age =st.text_input('Age of the passenger')              
+    SibSp =st.text_input('Number of Siblings/Spouses Aboard')              
+    Parch = st.text_input('Number of Parents/Children Aboard')                  
+    
+    
+    Result=''
+    
+    if st.button('Result'):
+        Result=titanic_prediction([ Pclass , Sex , Age , SibSp , Parch])
+        
+    st.success(Result)
+
+if __name__== '__main__':
+    main()
+    
+  
